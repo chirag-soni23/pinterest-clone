86851cdf07bea8f038549525a93b82978e800bbd
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-08 10:46:16 +0530

    pinterest-clone

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 const session = require("express-session");
 const passport = require('passport');
 const flash = require("connect-flash")
+const port = 5000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -52,5 +53,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+app.listen(port,()=>{
+  console.log(`http://localhost:${port}`)
+})
 
 module.exports = app;
